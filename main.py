c4d316054ff81458ff372711b3b81c747f6dd777
Tomasito <thomaskald849@gmail.com>
2025-04-22 16:32:17 +0300

    Created repository

---
+++ main.py
@@ -0,0 +1,7 @@
+import ahpy
+
+criteria_comparisons = {
+    ('Financial issues', 'Performance'): 1/3,
+    ('Financial issues', 'Social acceptance'): 2,
+    ('Performance', 'Social acceptance'): 4
+}
